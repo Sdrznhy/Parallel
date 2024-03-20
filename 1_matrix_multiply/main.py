5150d634925a2727a87f2f9c0d2b28b13801fcff
Sidereal <lsdznhy@gmail.com>
2024-03-20 03:20:05 +0000

    add python to compare with c++

---
+++ 1_matrix_multiply/main.py
@@ -0,0 +1,28 @@
+import time
+import numpy as np
+
+m = 777
+n = 787
+k = 747
+
+if __name__ == "__main__":
+    start = time.time()
+    # 生成m*n的矩阵A和n*k的矩阵B
+    A = np.random.rand(m, n)
+    B = np.random.rand(n, k)
+    C = np.dot(A, B)
+    end = time.time()
+    # 保留小数点后两位
+    print("m={}, n={}, k={}".format(m, n, k))
+    print("using numpy:")
+    print("Time: {:.2f}ms".format((end - start) * 1000))
+
+    start = time.time()
+    C = np.zeros((m, k))
+    for i in range(m):
+        for j in range(k):
+            for l in range(n):
+                C[i][j] += A[i][l] * B[l][j]
+    end = time.time()
+    print("using original python:")
+    print("Time: {:.2f}ms".format((end - start) * 1000))
---
+++ 1_matrix_multiply/record.md
@@ -0,0 +1,12 @@
+## using python
+
+```bash
+@Sdrznhy ➜ /workspaces/Parallel (main) $ /home/codespace/.python/current/bin/python3 /workspaces/Parallel/1_matrix_multiply/main.py
+m=777, n=787, k=747
+using numpy:
+Time: 29.50ms
+using original python:
+Time: 368050.73ms
+```
+
+## using C++
