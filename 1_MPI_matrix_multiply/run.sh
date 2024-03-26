c77aabd1cf1c1cbe29b3d81a36048161138cb16d
Sdrznhy <lsdznhy@gmail.com>
2024-03-26 23:42:03 +0800

    完成初步并行，待优化

--- 1_MPI_matrix_multiply/run.sh
+++ 1_MPI_matrix_multiply/run.sh
@@ -1,10 +1,11 @@
 echo "Running 1_MPI_matrix_multiply" 
-echo "Matrix_size | Number_of_Processes | Time" | tee  ./output/1_MPI_matrix_multiply.txt
+echo "Matrix_size | Number_of_Processes | Time" | tee -a  ./output/1_MPI_matrix_multiply.txt
 
 for size in 128 256 512 1024 2048
 do
-    for np in 1 2 4 8 16
+    for np in 1 2 4 8 
     do
-        mpirun --use-hwthread-cpus -np $np ./bin/main $size | tee -a ./output/1_MPI_matrix_multiply.txt
+        mpirun  -np $np ./bin/main $size | tee -a ./output/1_MPI_matrix_multiply.txt
     done
+    mpirun --use-hwthread-cpus -np 16 ./bin/main $size | tee -a ./output/1_MPI_matrix_multiply.txt
 done
--- 1_MPI_matrix_multiply/src/main.cpp
+++ 1_MPI_matrix_multiply/src/main.cpp
@@ -40,14 +40,11 @@ void matrixMultiply(double *matrixA, double *matrixB, double *matrixC, int row, 
 
 int main(int argc, char **argv)
 {
-    // only for square matrix
-
-    // to run
+    // 为了跑16个进程，需要在mpirun后加上--use-hwthread-cpus
     // mpic++ -o main main.cpp
-    // mpirun -np 4 ./main
-    // 好像还有别的编译指令，之后再加
+    // mpirun --use-hwthread-cpus -np 4 ./main
 
-    // 划定计算范围...?
+    // 划定计算范围
     // atoi: ASCII to Integer
     int n = atoi(argv[1]);     // 矩阵大小
     int beginRow, endRow;      // 每个进程的计算范围
@@ -63,18 +60,18 @@ int main(int argc, char **argv)
 
     MPI_Status status;
 
-    if (processNum == 1) // 非并行
+    if (processNum == 1) // 进程数为1，非并行
     {
         double *matrixA = matrixGenerate(n, n);
         double *matrixB = matrixGenerate(n, n);
         double *matrixC = matrixGenerateZero(n, n);
+
         beginTime = MPI_Wtime();
         matrixMultiply(matrixA, matrixB, matrixC, n, n);
         endTime = MPI_Wtime();
+
+        // Matrix_size | Number_of_Processes | Time
         std::cout << n << " " << processNum << " " << endTime - beginTime << std::endl;
-        // std::cout << "Size of matrix: " << n << std::endl;
-        // std::cout << "Time: " << endTime - beginTime << std::endl;
-        // std::cout << "Using process: " << processNum << std::endl;
 
         delete[] matrixA;
         delete[] matrixB;
@@ -84,6 +81,7 @@ int main(int argc, char **argv)
     else // 并行，进程0负责收发和汇总，其他进程负责计算
     {
         int rowsPerProcess = n / (processNum - 1);
+
         if (rank == 0) // 进程0，主进程
         {
             double *matrixA = matrixGenerate(n, n);
@@ -94,12 +92,16 @@ int main(int argc, char **argv)
 
             // 发送信息：A[beginRow:endRow] 和 B
             // beginRow = rowsPerProcess * (rank - 1)
-            // endRow = rowsPerProcess * rank
+            // endRow = rowsPerProcess * rank or n - 1
             for (int i = 0; i < processNum - 1; i++)
             {
                 beginRow = rowsPerProcess * i;
                 endRow = rowsPerProcess * (i + 1);
-                MPI_Send(&matrixA[beginRow * n + 0], rowsPerProcess * n, MPI_DOUBLE, i + 1, 0, MPI_COMM_WORLD);
+                if (i == processNum - 2)
+                    endRow = n;
+
+                MPI_Send(&beginRow, 1, MPI_INT, i + 1, 4, MPI_COMM_WORLD);
+                MPI_Send(&matrixA[beginRow * n + 0], (endRow - beginRow) * n, MPI_DOUBLE, i + 1, 0, MPI_COMM_WORLD);
                 MPI_Send(&matrixB[0 * n + 0], n * n, MPI_DOUBLE, i + 1, 1, MPI_COMM_WORLD);
             }
 
@@ -108,7 +110,10 @@ int main(int argc, char **argv)
             {
                 beginRow = rowsPerProcess * i;
                 endRow = rowsPerProcess * (i + 1);
-                MPI_Recv(&matrixC[beginRow * n + 0], rowsPerProcess * n, MPI_DOUBLE, i + 1, 2, MPI_COMM_WORLD, &status);
+                if (i == processNum - 2)
+                    endRow = n;
+
+                MPI_Recv(&matrixC[beginRow * n + 0], (endRow - beginRow) * n, MPI_DOUBLE, i + 1, 2, MPI_COMM_WORLD, &status);
             }
 
             endTime = MPI_Wtime();
@@ -124,16 +129,20 @@ int main(int argc, char **argv)
         }
         else // 其他进程
         {
-            // beginRow = rowsPerProcess * (rank - 1)
-            // endRow = rowsPerProcess * rank
-            double *matrixA = new double[rowsPerProcess * n];
-            double *matrixB = new double[n * n];
-            double *matrixC = new double[rowsPerProcess * n];
+            MPI_Recv(&beginRow, 1, MPI_INT, 0, 4, MPI_COMM_WORLD, &status);
+            if (rank == processNum - 1)
+                endRow = n;
+            else
+                endRow = rowsPerProcess * rank;
 
-            MPI_Recv(&matrixA[0 * n + 0], rowsPerProcess * n, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+            double *matrixA = new double[(endRow - beginRow) * n];
+            double *matrixB = new double[n * n];
+            double *matrixC = new double[(endRow - beginRow) * n];
+
+            MPI_Recv(&matrixA[0 * n + 0], (endRow - beginRow) * n, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
             MPI_Recv(&matrixB[0 * n + 0], n * n, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
 
-            matrixMultiply(matrixA, matrixB, matrixC, rowsPerProcess, n);
+            matrixMultiply(matrixA, matrixB, matrixC, endRow - beginRow, n);
 
             // 发送信息：C[beginRow:endRow]
             MPI_Send(&matrixC[0 * 0 + 0], rowsPerProcess * n, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
