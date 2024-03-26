a3f8fee510a0b5f6657cb1e5e58bfe7eee41b212
Sdrznhy <lsdznhy@gmail.com>
2024-03-26 17:18:42 +0800

    增加循环

--- 1_MPI_matrix_multiply/run.sh
+++ 1_MPI_matrix_multiply/run.sh
@@ -1,7 +1,10 @@
 echo "Running 1_MPI_matrix_multiply" 
-echo "Matrix_size | Number_of_Processes | Time" | tee ./output/1_MPI_matrix_multiply.txt
-mpirun --use-hwthread-cpus -np 1 ./bin/main 512 | tee -a ./output/1_MPI_matrix_multiply.txt
-mpirun --use-hwthread-cpus -np 2 ./bin/main 512 | tee -a ./output/1_MPI_matrix_multiply.txt
-mpirun --use-hwthread-cpus -np 4 ./bin/main 512 | tee -a ./output/1_MPI_matrix_multiply.txt
-mpirun --use-hwthread-cpus -np 8 ./bin/main 512 | tee -a ./output/1_MPI_matrix_multiply.txt
-mpirun --use-hwthread-cpus -np 16 ./bin/main 512 | tee -a ./output/1_MPI_matrix_multiply.txt
+echo "Matrix_size | Number_of_Processes | Time" | tee  ./output/1_MPI_matrix_multiply.txt
+
+for size in 128 256 512 1024 2048
+do
+    for np in 1 2 4 8 16
+    do
+        mpirun --use-hwthread-cpus -np $np ./bin/main $size | tee -a ./output/1_MPI_matrix_multiply.txt
+    done
+done
