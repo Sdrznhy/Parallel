b9bac65a573a1670cc5b027aa8854e481034e488
Sidereal <lsdznhy@gmail.com>
2024-04-28 15:23:54 +0800

    完成实验5代码

--- 5_OpenMP/Analyser.py
+++ 5_OpenMP/Analyser.py
@@ -3,42 +3,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-file = "output/MM_static.txt"
-# file = "output/MM_dynamic.txt"
+# file = "output/MM_static.txt"
+# file = "output/MM_dynamic_256.txt"
 # file = "output/MatrixMultiply.txt"
 # file = "output/MM_PthreadFor.txt"
 
-# 读取数据
-data = pd.read_csv(file, sep=" ", names=["Matrix_size", "Number_of_threads", "Time"])
 
-# 按照矩阵规模和进程数分组，计算平均时间
-grouped = data.groupby(["Matrix_size", "Number_of_threads"]).mean().reset_index()
+def analyse(file):
+    # 读取数据
+    data = pd.read_csv(
+        file, sep=" ", names=["Matrix_size", "Number_of_threads", "Time"]
+    )
 
-# 为每一行数据计算加速比，以每种规模的单线程时间为基准
-for size in grouped["Matrix_size"].unique():
-    base_time = grouped[
-        (grouped["Matrix_size"] == size) & (grouped["Number_of_threads"] == 1)
-    ]["Time"].values[0]
-    grouped.loc[grouped["Matrix_size"] == size, "Speedup"] = base_time / grouped["Time"]
+    # 按照矩阵规模和进程数分组，计算平均时间
+    grouped = data.groupby(["Matrix_size", "Number_of_threads"]).mean().reset_index()
 
-# 将矩阵规模用2的幂次方表示，并转换为字符串，方便绘图
-# grouped["Matrix_size"] = grouped["Matrix_size"].apply(lambda x: str(int(np.log2(x))))
+    # 为每一行数据计算加速比，以每种规模的单线程时间为基准
+    for size in grouped["Matrix_size"].unique():
+        base_time = grouped[
+            (grouped["Matrix_size"] == size) & (grouped["Number_of_threads"] == 1)
+        ]["Time"].values[0]
+        grouped.loc[grouped["Matrix_size"] == size, "Speedup"] = (
+            base_time / grouped["Time"]
+        )
 
-# 将矩阵规模转换为字符串，方便绘图
-grouped["Matrix_size"] = grouped["Matrix_size"].apply(lambda x: str(x))
+    # 将矩阵规模用2的幂次方表示，并转换为字符串，方便绘图
+    # grouped["Matrix_size"] = grouped["Matrix_size"].apply(lambda x: str(int(np.log2(x))))
 
-# 按照规模绘制加速比曲线
-sns.set(style="whitegrid")
-plt.figure()
-sns.lineplot(
-    data=grouped, x="Matrix_size", y="Speedup", hue="Number_of_threads", marker="o"
-)
-plt.title("Speedup powered by OpenMP")
-plt.xlabel("Matrix_size(log2)")
-plt.ylabel("Speedup")
-plt.legend(title="Number of Threads", loc="upper left")
+    # 将矩阵规模转换为字符串，方便绘图
+    grouped["Matrix_size"] = grouped["Matrix_size"].apply(lambda x: str(x))
 
-plt.show()
+    # 按照规模绘制加速比曲线
+    sns.set(style="whitegrid")
+    plt.figure()
+    sns.lineplot(
+        data=grouped, x="Matrix_size", y="Speedup", hue="Number_of_threads", marker="o"
+    )
+    # plt.title("OpenMP, schedule=dynamic, chunk=256")
+    # 去掉MM_前缀作为标题
+    title = file[7:-4]
+    title = title[3:]
+    plt.title(title)
+    # plt.title("Pthread For")
+    plt.xlabel("Matrix_size")
+    plt.ylabel("Speedup")
+    plt.legend(title="Number of Threads", loc="upper left")
 
+    # plt.show()
+    # 保存图片
+    plt.savefig("assets/{}.png".format(title))
+
+
+if __name__ == "__main__":
+    for file in [
+        "output/MM_static.txt",
+        "output/MM_dynamic_1.txt",
+        "output/MM_dynamic_4.txt",
+        "output/MM_dynamic_16.txt",
+        "output/MM_dynamic_64.txt",
+        "output/MM_dynamic_256.txt",
+        "output/MM_PthreadFor.txt",
+    ]:
+        analyse(file)
+
+    # analyse("output/MM_static.txt")
 # 使用pd输出markdown表格，只输出1024规模的数据
 # print(grouped[grouped["Matrix_size"] == "1024"].to_markdown(index=False))
--- 5_OpenMP/src/MatrixMultiply.cpp
+++ 5_OpenMP/src/MatrixMultiply.cpp
@@ -8,6 +8,7 @@
 
 #define DYNAMIC 1
 
+int chunkSize = 256;
 int numThreads;
 int size;
 
@@ -33,7 +34,7 @@ double** matrixMultiplyOpenMP(double** matrixA, double** matrixB, int rowA, int 
     }
 
 #if DYNAMIC
-#pragma omp parallel for num_threads(numThreads) collapse(2) schedule(dynamic)
+#pragma omp parallel for num_threads(numThreads) collapse(2) schedule(dynamic, chunkSize)
     for (int i = 0; i < rowA; i++) {
         for (int j = 0; j < colB; j++) {
             matrixC[i][j] = 0;
@@ -63,43 +64,43 @@ int main(int argc, char* argv[])
     size = argv[1] ? atoi(argv[1]) : 1000;
     numThreads = argv[2] ? atoi(argv[2]) : 4;
 
-    if (numThreads == 1) {
-        // 串行直接计算乘法
-        double** matrixA = matrixGenerate(size, size);
-        double** matrixB = matrixGenerate(size, size);
+    //     if (numThreads == 1) {
+    //         // 串行直接计算乘法
+    //         double** matrixA = matrixGenerate(size, size);
+    //         double** matrixB = matrixGenerate(size, size);
 
-        struct timeval start_time, end_time;
-        gettimeofday(&start_time, NULL);
+    //         struct timeval start_time, end_time;
+    //         gettimeofday(&start_time, NULL);
 
-        double** matrixC = new double*[size];
-        for (int i = 0; i < size; i++) {
-            matrixC[i] = new double[size];
-            for (int j = 0; j < size; j++) {
-                matrixC[i][j] = 0;
-                for (int k = 0; k < size; k++) {
-                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
-                }
-            }
-        }
+    //         double** matrixC = new double*[size];
+    //         for (int i = 0; i < size; i++) {
+    //             matrixC[i] = new double[size];
+    //             for (int j = 0; j < size; j++) {
+    //                 matrixC[i][j] = 0;
+    //                 for (int k = 0; k < size; k++) {
+    //                     matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+    //                 }
+    //             }
+    //         }
 
-        gettimeofday(&end_time, NULL);
+    //         gettimeofday(&end_time, NULL);
 
-        double time = (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
+    //         double time = (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
 
-// std::cout << size << " " << numThreads << " " << time << std::endl;
-#if DYNAMIC
-        std::ofstream file("output/MM_dynamic.txt", std::ios::app);
-#else
-        std::ofstream file("output/MM_static.txt", std::ios::app);
-#endif
-        if (!file) {
-            std::cerr << "File open error!" << std::endl;
-            return 0;
-        }
-        file << size << " " << numThreads << " " << time << std::endl;
-        file.close();
-        return 0;
-    }
+    // // std::cout << size << " " << numThreads << " " << time << std::endl;
+    // #if DYNAMIC
+    //         std::ofstream file("output/MM_dynamic.txt", std::ios::app);
+    // #else
+    //         std::ofstream file("output/MM_static.txt", std::ios::app);
+    // #endif
+    //         if (!file) {
+    //             std::cerr << "File open error!" << std::endl;
+    //             return 0;
+    //         }
+    //         file << size << " " << numThreads << " " << time << std::endl;
+    //         file.close();
+    //         return 0;
+    //     }
 
     struct timeval start_time, end_time;
 
@@ -117,7 +118,7 @@ int main(int argc, char* argv[])
 // std::cout << size << " " << numThreads << " " << time << std::endl;
 // 将结果输出到文件
 #if DYNAMIC
-    std::ofstream file("output/MM_dynamic.txt", std::ios::app);
+    std::ofstream file("output/MM_dynamic_" + std::to_string(chunkSize) + ".txt", std::ios::app);
 #else
     std::ofstream file("output/MM_static.txt", std::ios::app);
 #endif
